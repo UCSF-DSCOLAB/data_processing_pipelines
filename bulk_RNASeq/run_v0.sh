5766ab23da894e9ed1cd28c635fcc8d6e0ec0e5e
E Flynn <e.flynn@ucsf.edu>
2022-03-17 18:58:53 -0700

    updated code so that working dir goes to TMPDIR and is cleaned up after

--- bulk_RNASeq/run_v0.sh
+++ bulk_RNASeq/run_v0.sh
@@ -3,7 +3,7 @@
 #SBATCH --ntasks=1
 #SBATCH --mem=15G
 #SBATCH --time=24:00:00
-#SBATCH --output=/c4/home/%u/nextflow_run0_%j.log
+#SBATCH --output=/krummellab/data1/%u/logs/nextflow_run0_%j.log
 #SBATCH --partition=krummellab,common
 
 # Arugment:
@@ -11,6 +11,12 @@
 
 
 export PATH=$PATH:/krummellab/data1/ipi/software/nextflow/21.04.3_build_5560/
-nextflow run pipeline.nf -params-file $1 
 
+NXF_WORK=$TMPDIR/working/
+mkdir -p $NXF_WORK
+
+export $NXF_WORK
+export TMPDIR=$TMPDIR/tmp
+
+nextflow run pipeline.nf -params-file $1 
 

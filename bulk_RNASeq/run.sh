46019523ec858458aa3bf120c9e1f74507446ee5
E Flynn <e.flynn@ucsf.edu>
2022-05-24 11:47:07 -0700

    push of old changes

--- bulk_RNASeq/run.sh
+++ bulk_RNASeq/run.sh
@@ -2,7 +2,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --mem=15G
-#SBATCH --time=24:00:00
+#SBATCH --time=48:00:00
 #SBATCH --output=/krummellab/data1/%u/logs/nextflow_run_%j.log
 #SBATCH --partition=krummellab,common
 
@@ -12,6 +12,10 @@
 # pass as many additional arguments to nextflow as you'd like (e.g. -resume)
 
 export PATH=$PATH:/krummellab/data1/ipi/software/nextflow/21.04.3_build_5560/
+#NXF_WORK=/scratch/${USER}/nextflow/${SLURM_JOB_ID}/
+#mkdir -p $NXF_WORK
+#export $NXF_WORK
+
 nextflow run pipeline.nf -c $2 -params-file $1 "${@:3}"
 
 #nextflow run /krummellab/data1/erflynn/data_processing_pipelines/bulk_RNASeq/pipeline.nf -c $2 -params-file $1 -with-report $3 -with-trace -with-timeline $4

6bde20d7f329bc5e74f734d5f4c427364d1ccf51
E Flynn <e.flynn@ucsf.edu>
2022-05-24 12:57:56 -0700

    fixed clean up

--- bulk_RNASeq/run.sh
+++ bulk_RNASeq/run.sh
@@ -7,38 +7,30 @@
 #SBATCH --partition=krummellab,common
 
 # to run:
-#   sbatch ./run.sh <parameter_file>.yml [keep_work|remove_work]
+#   sbatch ./run.sh <parameter_file>.yml 
 
 # Arugments:
 # $1 is the parameter file: e.g. example/params.yml
-# $2 is whether to remove work directory ("keep_work" or "remove_work")
 # then pass as many additional arguments to nextflow as you'd like (e.g. -resume, -with-timeline)
+
+function cleanup()
+{
+    nf_work=/c4/scratch/${USER}/nextflow/${SLURM_JOB_ID}/
+    rm -rf ${nf_work}
+}
+trap cleanup EXIT
+
 
 export PATH=$PATH:/krummellab/data1/ipi/software/nextflow/21.04.3_build_5560/
 
 # check on arguments
 failed=false
-NUM_ARGS=$#
-
-if [ $NUM_ARGS -lt 2 ];
-then
- echo "Please run with at least two arguments. Usage:  sbatch ./run.sh <parameter_file>.yml [keep_work|remove_work]"
- failed=true;
-fi
-
 PARAM_FILE=$1
-WORK_ARG=$2
 
 if [ ! -f "$PARAM_FILE" ]; 
 then
  echo "Parameter file does not exist"
  failed=true;
-fi
-
-if [ "$WORK_ARG" != "remove_work" ] && [ "$WORK_ARG" != "keep_work" ];
-then
-  echo "The second argument must be either 'remove_work' or 'keep_work'"
-  failed=true;
 fi
 
 
@@ -52,12 +44,6 @@ else
     export NXF_WORK=${nf_work}
 
     # run the pipeline
-    nextflow run pipeline.nf -c tool.config -params-file $PARAM_FILE "${@:3}" 
-
-
-    # clean up if desired
-    if [ ${WORK_ARG} == "remove_work" ]
-    then
-	rm -rf ${nf_work}
-    fi
+    nextflow run pipeline.nf -c tool.config -params-file $PARAM_FILE "${@:2}" 
 fi
+

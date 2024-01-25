927531052e83727eba323eadfdec9debcab5cd70
Amadeo <6462800+amadeovezz@users.noreply.github.com>
2024-01-24 16:51:44 -0800

    write depooled seurat objects to individual files, named by their id

--- single_cell_RNAseq_snake/bin/de_pool.R
+++ single_cell_RNAseq_snake/bin/de_pool.R
@@ -14,29 +14,28 @@ load_seurat_objects <- function(file_paths) {
 }
 
 
-save_seurat_objects <- function(filename, seurat_list) {
+save_seurat_objects <- function(output_dir, seurat_list) {
   # Check if the provided list is actually a list of Seurat objects
   if (!is.list(seurat_list)) {
     stop("The provided 'seurat_list' is not a list.")
   }
 
-  for (obj in seurat_list) {
-    if (!inherits(obj, "Seurat")) {
-      stop("All elements of 'seurat_list' must be Seurat objects.")
-    }
+  # Ensure output directory exists
+  if (!dir.exists(output_dir)) {
+      dir.create(output_dir, recursive = TRUE)
   }
 
-  dir_to_create <- dirname(filename) # Extract the directory part of the filename
+  # Iterate through each Seurat object in the list
+  for (id in names(seurat_list)) {
+    obj <- seurat_list[[id]]
 
-  # Create the directory if it doesn't exist
-  if (!dir.exists(dir_to_create)) {
-      dir.create(dir_to_create, recursive = TRUE)
+    # Define the filename using the output directory and the ID
+    filename <- file.path(output_dir, paste0(id, ".rds"))
+
+    # Save the Seurat object
+    saveRDS(obj, file = filename)
   }
-
-  # Save the list of Seurat objects to a file
-  saveRDS(seurat_list, file = filename)
 }
-
 
 depool_by_id <- function(seurat_objects) {
   depooled_objs <- list()
@@ -77,13 +76,13 @@ args <- commandArgs(trailingOnly = TRUE)
 # All arguments except the last one are input files
 input_files <- args[1:(length(args) - 1)]
 # The last argument is the output file
-output_file <- args[length(args)]
+output_dir <- args[length(args)]
 
 cat("Input files:", input_files, "\n")
-cat("Output file:", output_file, "\n")
+cat("Output dir:", output_dir, "\n")
 cat("Loading seurat objects... \n")
 targets <- load_seurat_objects(input_files)
 cat("Depooling seurat_objects...\n" )
 depooled_objects <- depool_by_id(targets)
 cat("Saving depooled objects...\n")
-save_seurat_objects(output_file, depooled_objects)
+save_seurat_objects(output_dir, depooled_objects)
